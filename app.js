7e53c486262dea767e2f6f15628a4233000d7a4d
redahmeid <reda@redahmeid.com>
2018-01-12 11:44:25 +0000

    syntax error

--- app.js
+++ app.js
@@ -20,21 +20,7 @@ app.get('/', function(req, res) {
 res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/profile',function(req,res){
-  const userPool = CognitoSDK.CognitoUserPool({
-    UserPoolId: "us-east-1_DHdw9WBmW",
-    ClientId: "77s8qvi05mjegegfsbb6n7aid0"
-  });
-  const user = CognitoSDK.CognitoUser({ 'reda@redahmeid.com', userPool });
-  const authenticationData = { 'reda@redahmeid.com', 'Socrat3s' };
-  const authenticationDetails = CognitoSDK.AuthenticationDetails(authenticationData);
 
-  user.authenticateUser(authenticationDetails, {
-    onSuccess: result => console.log(result),
-    onFailure: err => console.log(err)
-  })
-
-})
 
 
 
