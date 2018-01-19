36e8b1fbf58292015cd16276b1aa1181d41a8eb7
redahmeid <reda@redahmeid.com>
2018-01-19 12:42:35 +0000

    somehow broken this

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var AuthenticationClient = require('auth0').AuthenticationClient;
-var webAuth = new auth0.WebAuth({
-    domain:       'openlight.eu.auth0.com',
-    clientID:     'xeTjOnEzrgCZO1ZdCDubNDfHn1om4Ijg'
-  });
+
 
 var app = express();
 
 
-const queryString = require('query-string');
 app.get('/profile', function(req, res) {
  res.sendFile(__dirname + '/login.html');
 });
